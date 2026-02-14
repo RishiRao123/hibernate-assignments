3bb452e4c1215e4e7743eadec82b2008f6929f38
RishiRao123 <rishiraopuskuri@gmail.com>
2026-02-14 14:40:17 +0530

    Entity and hibernate util added

---
+++ assignment1_hibernate/src/main/java/entity/Product.java
@@ -0,0 +1,66 @@
+package entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private  String name;
+
+    private String description;
+
+    private String category;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Column(name = "price", nullable = false)
+    private double price;
+
+    @Column(unique = true)
+    private  String sku;
+
+    private boolean isActive;
+
+    public Product() {}
+
+    public Product(String name, String description, String category, int quantity, double price, String sku, boolean isActive) {
+        this.name = name;
+        this.description = description;
+        this.category = category;
+        this.quantity = quantity;
+        this.price = price;
+        this.sku = sku;
+        this.isActive = isActive;
+    }
+
+    public Long getId() { return id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public String getCategory() { return category; }
+    public void setCategory(String category) { this.category = category; }
+
+    public int getQuantity() { return quantity; }
+    public void setQuantity(int quantity) { this.quantity = quantity; }
+
+    public double getPrice() { return price; }
+    public void setPrice(double price) { this.price = price; }
+
+    public String getSku() { return sku; }
+    public void setSku(String sku) { this.sku = sku; }
+
+    public boolean getIsActive() { return isActive; }
+    public void setActive(boolean active) { this.isActive = active; }
+
+}
